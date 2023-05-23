3efcb089e56ab88abcd05e9ef229aee6f64a376b
Шухрат Мамаримов <shuhrat.mamarimov@pitch-it.ru>
2023-05-23 14:25:29 +0300

    1000 стили, компонент прогресс бара

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,15 @@
 <template lang="pug">
   .page.index.wrapper
     div body
+    progressBar
 </template>
 
 <script>
+import progressBar from '@components/progress-bar'
+
 export default {
   name: 'page-index',
-  components: {}
+  components: { progressBar }
 }
 </script>
 
